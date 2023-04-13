6d818e473f07940153aa2afb1e4888e7b8bb094f
Jura19777 <poryadin77@gmail.com>
2023-04-13 15:01:08 +0700

    Initial commit

---
+++ task1.py
@@ -0,0 +1,25 @@
+"""
+реализовать дескрипторы для любых двух классов
+"""
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    class RadiusDescriptor:
+        def __get__(self, instance, owner):
+            return instance._radius
+
+        def __set__(self, instance, value):
+            if value <= 0:
+                raise ValueError("Радиус должен быть положительным числом")
+            instance._radius = value
+
+    radius = RadiusDescriptor()
+
+c = Circle(5)
+print(c.radius)
+c.radius = 10
+print(c.radius)
+c.radius = -5
+print(c.radius)
---
+++ task2.py
@@ -0,0 +1,19 @@
+"""
+реализовать метакласс. позволяющий создавать всегда один и тот же объект класса
+"""
+class Singleton(type):
+    __instance__ = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls.__instance__ is None:
+            cls.__instance__ = super().__call__(*args, **kwargs)
+        return cls.__instance__
+
+class MyClass(metaclass=Singleton):
+    pass
+
+a = MyClass()
+b = MyClass()
+print(a is b)
+print(id(a))
+print(id(b))
